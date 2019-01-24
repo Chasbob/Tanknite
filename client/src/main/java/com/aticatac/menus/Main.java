857738b82e5903d3e10bce037b71dadcc68b2040
Charles de Freitas <ypph279@gmail.com>
2019-01-24 14:32:19 +0000

    Add implementation of java11
    Currently compiles but javafx doesn't seem to work properly.

--- client/src/main/java/com/aticatac/menus/Main.java
+++ client/src/main/java/com/aticatac/menus/Main.java
@@ -1,10 +1,16 @@
 package com.aticatac.menus;
 
+import javafx.stage.Stage;
+
 public class Main {
 
     public static void main(String[] args) {
         UIController UI = new UIController();
-
+        try {
+            UI.start(new Stage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- client/src/main/java/com/aticatac/menus/UIController.java
+++ client/src/main/java/com/aticatac/menus/UIController.java
@@ -6,11 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class UIController extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Main.class.getClassLoader().getResource("sample.fxml"));
+        URL resource =  getClass().getResource("");
+        System.out.println(resource.getPath());
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("AticAtac");
         primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
