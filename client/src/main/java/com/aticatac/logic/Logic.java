63935b715064248f342e1fbefe5ba7989d5bcb84
ben crocker <bjc756@student.bham.ac.uk>
2019-01-27 18:00:37 +0000

    Added methods

--- client/src/main/java/com/aticatac/logic/Logic.java
+++ client/src/main/java/com/aticatac/logic/Logic.java
@@ -1,5 +1,52 @@
 package com.aticatac.logic;
-// Test
 public class Logic {
+    public static void createNewTank(){
+// determine whether ai, put behavioural trees from ai if so
+    }
 
+    public static void moveForward(){
+        // Talk to physics, it will return whether can move, if can then tell renderer it moves and where
+    }
+
+    public static void turnLeft() {
+
+    }
+
+    public static void turnRight() {
+
+    }
+
+    public static void shoot() {
+// create bullet object
+    }
+
+    public static void isShot() {
+// part of colliding? Physics calls this method?
+        //if new health = 0 then die()
+    }
+
+    public static void die () {
+        if (numberOfAliveTanks() == 1){
+            gameFinish();
+        }
+    }
+
+    public static void getHealth () {
+
+    }
+
+    public static void gameFinish () {
+// only one tank left
+    }
+
+    public static int numberOfAliveTanks () {
+        int numberOfTanks = 0;
+        return numberOfTanks;
+    }
+
+/*Potential:
+PickUpItem
+Reload
+getAmmo()
+ */
 }
