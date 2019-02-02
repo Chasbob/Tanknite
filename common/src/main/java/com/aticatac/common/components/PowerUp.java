483ac013c72e5778b6d5de99704ceef65308e53f
cxf717 <cxf717@cs.bham.ac.uk>
2019-02-02 23:27:12 +0000

    Powerups component

---
+++ common/src/main/java/com/aticatac/common/components/PowerUp.java
@@ -0,0 +1,21 @@
+package com.aticatac.common.components;
+
+import com.aticatac.common.objectsystem.GameObject;
+
+public class PowerUp extends Component {
+
+    public PowerUp (GameObject parent) {
+        super(parent);
+    }
+
+    //this is a component for all powerups
+
+    //powerups include:
+    // speed (increases by 10%),
+    
+
+    // bullet power up (increase damage by 20%),
+    // ammo power up (increases ammo by 10 bullets),
+    // health power up (increases health by 10%)
+
+}
