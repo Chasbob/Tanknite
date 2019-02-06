9938adf2d81ca00fc115243de56dbfe9a480155b
Charles de Freitas <cxd738@student.bham.ac.uk>
2019-02-06 16:29:08 +0000

    Add libgdx and desktop module.
    JavaFX code and dependencies have been removed. Desktop is intended for running the client in a desktop environment.

--- client/src/main/java/com/aticatac/client/menus/Main.java
+++
@@ -1,9 +0,0 @@
-package com.aticatac.client.menus;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(UIController.class, args);
-    }
-}
