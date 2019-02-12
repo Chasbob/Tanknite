66b58d835e7d09692cb53bacd4515885cc9b528d
cxf717 <cxf717@cs.bham.ac.uk>
2019-02-12 13:35:12 +0000

    Added java docs and fixed gets for Position

--- common/src/main/java/com/aticatac/common/components/transform/Position.java
+++ common/src/main/java/com/aticatac/common/components/transform/Position.java
@@ -1,7 +1,39 @@
 package com.aticatac.common.components.transform;
 
 public class Position {
-    public Position(){x = 0;y=0;}
-    public Position(double xs,double ys){x = xs;y=ys;}
+
+    /**The x and y coordinates for the position*/
     public double x,y;
+
+
+    /**
+     * Constructor for a position where the values are not yet set
+     */
+    public Position(){x = 0;y=0;}
+
+
+    /**
+     * Constructor for a position where the values are known
+     * @param xs the X value
+     * @param ys the Y value
+     */
+    public Position(double xs,double ys){x = xs;y=ys;}
+
+
+    /**
+     * Gets the x value for the position
+     * @return the X value
+     */
+    public double getX(){
+        return x;
+    }
+
+
+    /**
+     * Gets the Y value for the position
+     * @return the Y value
+     */
+    public double getY(){
+        return y;
+    }
 }
