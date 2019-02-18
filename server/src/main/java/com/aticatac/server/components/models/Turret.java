c182b5e722a27f58c8802baf3d05a192b3b25ae3
Ben Crocker <bencrocker22@outlook.com>
2019-02-18 13:33:03 +0000

    Comments and method changes

--- server/src/main/java/com/aticatac/server/components/models/Turret.java
+++ server/src/main/java/com/aticatac/server/components/models/Turret.java
@@ -4,4 +4,13 @@ public class Turret {
 
     // does the shooting etc, linked to main tank
     // able to shoot 360 degrees
+    // game object or component?
+    // child of tank
+    // has transform component for rotation
+    // move same as tank?
+
+    // shoot, take rotation of turret from transform component as an angle and send as param to physics
+
+    double currentRotation = this.getComponent(Transform.class).getRotation();
+
 }
