3b6c75814bd00d1ed1d8bfa6069461f8553ea782
cxf717 <cxf717@cs.bham.ac.uk>
2019-02-18 14:25:59 +0000

    Addition of server data

--- server/src/main/java/com/aticatac/server/components/PhysicsManager.java
+++ server/src/main/java/com/aticatac/server/components/PhysicsManager.java
@@ -46,7 +46,7 @@ public class PhysicsManager extends Component {
     private Position move(String direction){
 
         //the position for this tank.
-        Position position = componentParent.getComponent(Transform.class).GetPosition();
+        Position position = componentParent.getComponent(Transform.class).getPosition();
         //Positions of everything
         //TODO make this get the server component from a different object not this one
         ArrayList<Position> occupiedCoordinates = componentParent.findObject("String").getComponent(Server.class).getOccupiedCoordinates();
@@ -139,7 +139,7 @@ public class PhysicsManager extends Component {
 
     public Position bulletMove(double bearing){
 
-        Position position = componentParent.getComponent(Transform.class).GetPosition();
+        Position position = componentParent.getComponent(Transform.class).getPosition();
         double xCoord = position.getX();
         double yCoord = position.getY();
 
@@ -179,7 +179,7 @@ public class PhysicsManager extends Component {
     private boolean collision(Position newPosition, Position oldPosition){
 
         //TODO make this get the server component from a different object not this one
-        ArrayList<Position> occupiedCoordinates = componentParent.findObject("String").getComponent(Server.class).getOccupiedCoordinates();
+        ArrayList<Position> occupiedCoordinates = componentParent.findObject("String").getComponent(ServerData.class).getOccupiedCoordinates();
 
         //checks for collisions
         for(int i=0; i<occupiedCoordinates.size(); i++) {
---
+++ server/src/main/java/com/aticatac/server/components/ServerData.java
@@ -0,0 +1,11 @@
+package com.aticatac.server.components;
+
+import com.aticatac.common.components.Component;
+import com.aticatac.common.objectsystem.GameObject;
+
+public class ServerData extends Component {
+    //server will have all of the updated and synchronised data
+    public ServerData(GameObject parent) {
+        super(parent);
+    }
+}
