ebb882887d3440f323d93731edf5165ecd393b08
cxf717 <cxf717@cs.bham.ac.uk>
2019-02-19 16:41:51 +0000

    Fixed Score

--- server/src/main/java/com/aticatac/server/components/Score.java
+++ server/src/main/java/com/aticatac/server/components/Score.java
@@ -1,8 +1,9 @@
 package com.aticatac.server.components;
 
+import com.aticatac.common.components.Component;
 import com.aticatac.common.objectsystem.GameObject;
 
-public class Score extends Component{
+public class Score extends Component {
     public int Score = 0;
 
     public Score(GameObject componentParent) {
