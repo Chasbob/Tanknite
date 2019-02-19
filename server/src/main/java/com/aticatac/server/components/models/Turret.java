e26fb618ac492b4da6d1a543a1ca6a8cce283331
Ben Crocker <bencrocker22@outlook.com>
2019-02-19 12:00:52 +0000

    Added support for 360 degree shooting

--- server/src/main/java/com/aticatac/server/components/models/Turret.java
+++ server/src/main/java/com/aticatac/server/components/models/Turret.java
@@ -1,16 +1,35 @@
 package com.aticatac.server.components.models;
 
-public class Turret {
+import com.aticatac.common.objectsystem.GameObject;
+import com.aticatac.server.components.Position;
+import com.aticatac.server.components.Transform;
 
-    // does the shooting etc, linked to main tank
-    // able to shoot 360 degrees
-    // game object or component?
-    // child of tank
-    // has transform component for rotation
-    // move same as tank?
+public class Turret extends GameObject {
 
-    // shoot, take rotation of turret from transform component as an angle and send as param to physics
+    public Turret (GameObject Parent, String name){
 
-    double currentRotation = this.getComponent(Transform.class).getRotation();
+        super(Parent, name);
+        this.addComponent(Transform.class);
+        this.addComponent(Position.class);
+
+        // does the shooting etc, linked to main tank
+        // able to shoot 360 degrees
+        // game object or component?
+        // child of tank
+        // has transform component for rotation
+        // move same as tank?
+
+        // shoot, take rotation of turret from transform component as an angle and send as param to physics
+
+        double currentRotation = this.getComponent(Transform.class).getRotation();
+    }
+
+    public boolean shoot () {
+
+        Bullet bullet = new Bullet(Transform.getX(), Transform.getY(), Transform.getRotation()); // NEED TO CHANGE PARAMS FOR BULLET
+        bullet.moveForwards();
+
+
+    }
 
 }
