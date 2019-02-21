a2c17494f92bc56c4fa0672eca99c5efccea24c8
Ben Crocker <bjc756@student,bham.ac.uk>
2019-02-21 14:23:45 +0000

    Changes from dev

--- common/src/main/java/com/aticatac/common/objectsystem/GameObject.java
+++ common/src/main/java/com/aticatac/common/objectsystem/GameObject.java
@@ -6,6 +6,7 @@ import com.aticatac.common.components.transform.Transform;
 import com.aticatac.common.exceptions.ComponentExistsException;
 import com.aticatac.common.exceptions.InvalidClassInstance;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Optional;
 
--- server/src/main/java/com/aticatac/server/components/controller/TurretController.java
+++ server/src/main/java/com/aticatac/server/components/controller/TurretController.java
@@ -1,30 +1,26 @@
 package com.aticatac.server.components.controller;
 
-import com.aticatac.common.components.Ammo;
 import com.aticatac.common.components.Component;
 import com.aticatac.common.objectsystem.GameObject;
+import com.aticatac.server.prefabs.BulletObject;
 
 public class TurretController extends Component {
-  /**
-   * Instantiates a new Component.
-   *
-   * @param gameObject the component parent
-   */
-  public TurretController(GameObject gameObject) {
-    super(gameObject);
-  }
+    /**
+     * Instantiates a new Component.
+     *
+     * @param gameObject the component parent
+     */
+    public TurretController(GameObject gameObject) {
+        super(gameObject);
+    }
 
-  public boolean shoot() {
-    //TODO shooting
-    BulletController bullet = new BulletController(this.getGameObject());
-    bullet.moveForwards();
-    //moves forwards until collision happens then it will be destroyed.
-    //constantly check if collided() method is returning true
-    int currentAmmo = this.getGameObject().getComponent(Ammo.class).getAmmo();
-//
-//        if (currentAmmo == 0) return false;
-//        return this.findObject(TurretController,) // get turret for this particular tank, and call shoot method in it
-//        this.getComponent(Ammo.class).setAmmo(currentAmmo - 1);
-    return true;
-  }
+    public boolean shoot () {
+        //TODO shooting
+        BulletObject bullet = new BulletObject(this.getGameObject());
+        bullet.getComponent(BulletController.class).moveForwards();
+        //moves forwards until collision happens then it will be destroyed.
+        //constantly check if collided() method is returning true
+        return true;
+    }
+
 }
