39d5e249ecdca3dc0f934cc6d4c9694516251ec9
Claire <cxf717@student.bham.ac.uk>
2019-03-04 14:23:31 +0000

    Added tank not moving below 10 health and Power up components

--- server/src/main/java/com/aticatac/server/components/BulletPowerUp.java
+++ server/src/main/java/com/aticatac/server/components/BulletPowerUp.java
@@ -7,12 +7,27 @@ import com.aticatac.common.objectsystem.GameObject;
  *
  */
 public class BulletPowerUp extends Component {
-  /**
-   * Creates a new bullet power up with a parent
-   *
-   * @param gameObject
-   */
-  public BulletPowerUp(GameObject gameObject) {
-    super(gameObject);
-  }
+
+    /**New damage this power up provides*/
+    private int damage = 20;
+
+    /**
+     * Creates a new bullet power up with a parent
+     * @param gameObject
+     */
+    public BulletPowerUp(GameObject gameObject) {
+        super(gameObject);
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public int getBulletDamage(){
+
+        return damage;
+    }
+
+
 }
