6540e203eae21775c17b3081b583659a9ac53159
Claire <cxf717@student.bham.ac.uk>
2019-03-07 10:31:02 +0000

    Addition of audio into physics

--- client/src/main/java/com/aticatac/client/util/SoundManager.java
+++ client/src/main/java/com/aticatac/client/util/SoundManager.java
@@ -1,4 +1,64 @@
 package com.aticatac.client.util;
 
 public class SoundManager {
+
+  private boolean soundOn = true;
+
+  /**
+   *
+   */
+  public void onShooting(){
+
+    if(soundOn) {
+      AudioEnum.INSTANCE.getShoot().play();
+    }
+
+  }
+
+  /**
+   *
+   */
+  public void onTankMove(){
+
+    if(soundOn) {
+      AudioEnum.INSTANCE.getTankMove().play();
+    }
+
+  }
+
+  /**
+   *
+   */
+  public void onPowerUp(){
+
+    if(soundOn) {
+      AudioEnum.INSTANCE.getPowerUp().play();
+    }
+
+  }
+
+  /**
+   *
+   */
+  public void onTankDeath(){
+
+    if(soundOn) {
+      AudioEnum.INSTANCE.getTankDeath().play();
+    }
+
+  }
+
+  /**
+   *
+   */
+  public void onButtonClick(){
+
+    if(soundOn) {
+      AudioEnum.INSTANCE.getButtonClick().play();
+    }
+
+  }
+
+
+
 }
