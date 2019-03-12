5e201ec3930339a69ac70169dad634f5aa93db3f
Charlie de Freitas <cxd738@student.bham.ac.uk>
2019-03-12 16:37:42 +0000

    Update multiplayer hosting.

--- client/src/main/java/com/aticatac/client/screens/GameScreen.java
+++ client/src/main/java/com/aticatac/client/screens/GameScreen.java
@@ -209,7 +209,7 @@ public class GameScreen extends AbstractScreen {
       }
     }
     tanks.end();
-    if (update != null) {
+    if (update != null && update.getMe(Data.INSTANCE.getID()) != null) {
       hudUpdate.update(update);
     }
     batch.begin();
